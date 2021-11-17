72e903e9204f4540616ef8473f479b6705b7f548
Kevin C. Krinke <kevin@krinke.ca>
2021-11-17 12:20:19 -0500

    ctype_item.go: implement private read/write locking for type-specific things (unexported locking); expose RWMutex methods for normal concrete type item locking that includes read locking ability

--- ctype_item.go
+++ ctype_item.go
@@ -23,8 +23,6 @@ import (
 )
 
 type TypeItem interface {
-	sync.Locker
-
 	InitTypeItem(tag TypeTag, thing interface{}) (already bool)
 	Init() (already bool)
 	IsValid() bool
@@ -43,17 +41,23 @@ type TypeItem interface {
 	LogWarn(format string, argv ...interface{})
 	LogError(format string, argv ...interface{})
 	LogErr(err error)
+
+	Lock()
+	Unlock()
+	RLock()
+	RUnlock()
 }
 
 type CTypeItem struct {
-	sync.Mutex
-
 	id       uuid.UUID
 	typeTag  CTypeTag
 	name     string
 	ancestry []TypeTag
 	valid    bool
 	self     interface{}
+	typeLock *sync.RWMutex
+
+	sync.RWMutex
 }
 
 func NewTypeItem(tag CTypeTag, name string) TypeItem {
@@ -64,35 +68,47 @@ func NewTypeItem(tag CTypeTag, name string) TypeItem {
 		ancestry: make([]TypeTag, 0),
 		valid:    false,
 		self:     nil,
+		typeLock: &sync.RWMutex{},
 	}
 }
 
 func (o *CTypeItem) InitTypeItem(tag TypeTag, thing interface{}) (already bool) {
-	o.Lock()
-	defer o.Unlock()
+	if o.typeLock == nil {
+		o.typeLock = &sync.RWMutex{}
+	}
+	o.typeLock.RLock()
 	already = o.valid
 	if !already {
 		if o.typeTag == TypeNil {
 			o.typeTag = tag.Tag()
 		}
 		if o.id == uuid.Nil {
+			o.typeLock.RUnlock()
+			o.typeLock.Lock()
 			var err error
 			if o.id, err = TypesManager.AddTypeItem(o.typeTag, thing); err != nil {
 				log.FatalDF(1, "failed to add self to \"%v\" type: %v", o.typeTag, err)
 			} else {
 				o.self = thing
 			}
+			o.typeLock.Unlock()
 		} else {
+			o.typeLock.RUnlock()
+			o.typeLock.Lock()
 			o.ancestry = append(o.ancestry, tag)
+			o.typeLock.Unlock()
 		}
+	} else {
+		o.typeLock.RUnlock()
 	}
 	return
 }
 
 func (o *CTypeItem) Init() (already bool) {
-	if o.valid {
+	if o.IsValid() {
 		return true
 	}
+	o.typeLock.RLock()
 	if o.typeTag == TypeNil {
 		log.FatalDF(1, "invalid object type: nil")
 	}
@@ -100,25 +116,35 @@ func (o *CTypeItem) Init() (already bool) {
 	if TypesManager.HasType(o.typeTag) {
 		for _, i := range TypesManager.GetTypeItems(o.typeTag) {
 			if c, ok := i.(TypeItem); ok {
+				o.typeLock.RUnlock()
 				if c.ObjectID() == o.ObjectID() {
+					o.typeLock.RLock()
 					found = true
 					break
 				}
+				o.typeLock.RLock()
 			}
 		}
 	}
 	if !found {
 		log.FatalDF(1, "type or instance not found: %v (%v)", o.ObjectName(), o.typeTag)
 	}
+	o.typeLock.RUnlock()
+	o.typeLock.Lock()
+	defer o.typeLock.Unlock()
 	o.valid = true
 	return false
 }
 
 func (o *CTypeItem) IsValid() bool {
+	o.typeLock.RLock()
+	defer o.typeLock.RUnlock()
 	return o.valid && o.self != nil
 }
 
 func (o *CTypeItem) Self() (this interface{}) {
+	o.typeLock.RLock()
+	defer o.typeLock.RUnlock()
 	return o.self
 }
 
@@ -127,44 +153,60 @@ func (o *CTypeItem) String() string {
 }
 
 func (o *CTypeItem) GetTypeTag() TypeTag {
+	o.typeLock.RLock()
+	defer o.typeLock.RUnlock()
 	return o.typeTag
 }
 
 func (o *CTypeItem) GetName() string {
+	o.typeLock.RLock()
+	defer o.typeLock.RUnlock()
 	return o.name
 }
 
 func (o *CTypeItem) SetName(name string) {
-	o.Lock()
-	defer o.Unlock()
+	o.typeLock.Lock()
+	defer o.typeLock.Unlock()
 	o.name = name
 }
 
 func (o *CTypeItem) ObjectID() uuid.UUID {
+	o.typeLock.RLock()
+	defer o.typeLock.RUnlock()
 	return o.id
 }
 
 func (o *CTypeItem) ObjectName() string {
+	o.typeLock.RLock()
+	tt, n := o.typeTag, o.name
+	o.typeLock.RUnlock()
 	if len(o.name) > 0 {
-		return fmt.Sprintf("%v-%v#%v", o.typeTag, o.ObjectID(), o.name)
+		return fmt.Sprintf("%v-%v#%v", tt, o.ObjectID(), n)
 	}
-	return fmt.Sprintf("%v-%v", o.typeTag, o.ObjectID())
+	return fmt.Sprintf("%v-%v", n, o.ObjectID())
 }
 
 func (o *CTypeItem) DestroyObject() (err error) {
+	o.RLock()
 	if err = TypesManager.RemoveTypeItem(o.typeTag, o); err != nil {
 		o.LogErr(err)
 	}
+	o.RUnlock()
+	o.typeLock.Lock()
+	defer o.typeLock.Unlock()
 	o.valid = false
 	o.id = uuid.Nil
 	return nil
 }
 
 func (o *CTypeItem) LogTag() string {
+	o.typeLock.RLock()
+	tt, n := o.typeTag, o.name
+	o.typeLock.RUnlock()
 	if len(o.name) > 0 {
-		return fmt.Sprintf("[%v.%v.%v]", o.typeTag, o.ObjectID(), o.name)
+		return fmt.Sprintf("[%v.%v.%v]", tt, o.ObjectID(), n)
 	}
-	return fmt.Sprintf("[%v.%v]", o.typeTag, o.ObjectID())
+	return fmt.Sprintf("[%v.%v]", tt, o.ObjectID())
 }
 
 func (o *CTypeItem) LogTrace(format string, argv ...interface{}) {
