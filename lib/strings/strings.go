fe907b12ff267115a780f80a9943a21a4328d9eb
Kevin C. Krinke <kevin@krinke.ca>
2023-04-17 17:23:34 -0400

    lib/strings: updates to StringIsDomainName parsing

--- lib/strings/strings.go
+++ lib/strings/strings.go
@@ -159,9 +159,16 @@ func MakeObjectName(tag, user, address string) (name string) {
 	return
 }
 
-var _rxDomainName = regexp.MustCompile(`^(?i)[a-z\d][-a-z\d]+?[a-z\d]+(\.[a-z\d][-a-z\d]+?[a-z\d]+?)+\.?$`)
+var _rxDomainName = regexp.MustCompile(`^(?i)[a-z\d][-a-z\d]+?(\.[a-z\d]+?)*\.[a-z\d]{2,6}\.?$`)
 
 func StringIsDomainName(input string) bool {
+	/*
+		The domain name should be a-z or A-Z or 0-9 and hyphen (-).
+		The domain name should be between 1 and 63 characters long.
+		The domain name should not start or end with a hyphen(-) (e.g. -geeksforgeeks.org or geeksforgeeks.org-).
+		The last TLD (Top level domain) must be at least two characters and a maximum of 6 characters.
+		The domain name can be a subdomain (e.g. write.geeksforgeeks.org).
+	*/
 	return _rxDomainName.MatchString(input)
 }
 
